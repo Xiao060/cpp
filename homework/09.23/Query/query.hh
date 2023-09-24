8993b81e056b32dc5029fe1597e551a922b2f1d4
xiao <liangshengxiao1@163.com>
2023-09-24 10:39:23 +0800

    1

---
+++ homework/09.23/Query/query.hh
@@ -0,0 +1,59 @@
+#ifndef __QUERY_HPP__
+#define __QUERY_HPP__
+
+class QueryBase {
+   
+public:
+   
+private:
+   
+};
+
+class WordQuery 
+: public QueryBase {
+   
+public:
+   
+private:
+   
+};
+
+class NotQuery 
+: public  QueryBase {
+   
+public:
+   
+private:
+   
+};
+
+class BinaryQuery 
+: public QueryBase {
+   
+public:
+   
+private:
+   
+};
+
+class AndQuery 
+: public BinaryQuery {
+   
+public:
+   
+private:
+   
+};
+
+class OrQuery 
+: public BinaryQuery {
+   
+public:
+   
+private:
+   
+};
+
+
+
+#endif

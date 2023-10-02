33b4e2f0f81b8bd881f6daf444033adfa472b295
xiao <liangshengxiao1@163.com>
2023-10-02 19:39:08 +0800

    1

---
+++ homework/10.02/self_vector.cc
@@ -0,0 +1,37 @@
+#include "self_vector.hh"
+
+template<typename T>
+Vector<T>::Vector() {
+
+}
+
+template<typename T>
+Vector<T>::~Vector() {
+
+}
+
+template<typename T>
+void Vector<T>::push_back(const T &) {
+
+}
+
+template<typename T>
+void Vector<T>::pop_back() {
+
+}
+
+template<typename T>
+int Vector<T>::size() {
+
+}
+
+template<typename T>
+int Vector<T>::capacity() {
+
+}
+
+//重新分配内存,动态扩容要用的 
+template<typename T>
+void Vector<T>::reallocate() {
+
+}

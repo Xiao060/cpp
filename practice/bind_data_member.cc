ae066b3a2c56693b6d2e1fa112a37853890a5dee
xiao <liangshengxiao1@163.com>
2023-10-04 15:29:48 +0800

    1

--- practice/bind_data_member.cc
+++ practice/bind_data_member.cc
@@ -16,11 +16,17 @@ public:
     int data = 100;
 };
 
+int add(int x, int y) {
+    return x + y;
+}
 
 int main() {
     Example ex(12);
     auto f = bind(&Example::data, &ex);
     
+    auto f1 = bind(&add, 10, 20);
+
+    std::function<int()> f2 = bind(&add, 10, 20);
 
     cout << f() << endl;
 

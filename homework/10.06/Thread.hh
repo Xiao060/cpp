237c176e10598832a3b3b67908ad6e69e3b6418d
xiao <liangshengxiao1@163.com>
2023-10-08 20:53:06 +0800

    1

--- homework/10.06/Thread.hh
+++ homework/10.06/Thread.hh
@@ -1,10 +1,34 @@
 #ifndef __THREAD_HPP__
 #define __THREAD_HPP__
 
-class Thread {
+#include <iostream>
+#include <pthread.h>
 
+using std::cout;
+using std::endl;
+
+/*****************************************************************************/
+/*                               头文件                                      */
+/****************************************************************************/
+
+class Thread {
 public:
+    Thread();
+    virtual ~Thread();
+
+    void start();
+    void stop();
+
+private:
+    // pthread_create 第三个参数 类型为 参数为 void*, 返回值为 void* 的函数指针
+    // 若 threadFunc 作为 非静态 成员函数, 则 至少有一个参数 this 指针
+    // 故 应该把 threadFunc 设为 静成员函数
+    static void* threadFunc(void* args);
     virtual void run() = 0;
+
+    pthread_t _thid;
+    bool _isRunning;
 };
+
 
 #endif
--- homework/10.06/main.cc
+++ homework/10.06/main.cc
@@ -1,48 +1,26 @@
 #include "Consumer.hh"
 #include "Producer.hh"
 #include "TaskQueue.hh"
+#include "Thread.hh"
 #include <iostream>
 #include <pthread.h>
 
-void* proThreadFunc(void * args);
-void* conThreadFunc(void * args);
 
 int main(int argc, char* argv[]) {
 
     TaskQueue tq(10);
 
-    Producer pro(tq);
-    Consumer con(tq);
+    Thread* pro1 = new Producer(tq);
+    Thread* pro2 = new Producer(tq);
+    Thread* con1 = new Consumer(tq);
 
-    pthread_t pro1;    
-    pthread_t con1, con2;    
+    pro1->start();
+    pro2->start();
+    con1->start();
 
-    pthread_create(&pro1, nullptr, proThreadFunc, &pro);
-
-    pthread_create(&con1, nullptr, conThreadFunc, &con);
-    pthread_create(&con2, nullptr, conThreadFunc, &con);
-
-    pthread_join(pro1, nullptr);
-    pthread_join(con2, nullptr);
-    pthread_join(con2, nullptr);
+    pro1->stop();
+    pro2->stop();
+    con1->stop();
 
     return 0;
-}
-
-
-void* proThreadFunc(void * args) {
-    Producer* ppro = (Producer*) args;
-
-    ppro->run();
-
-    return nullptr;
-}
-
-
-void* conThreadFunc(void * args) {
-    Consumer* pcon = (Consumer*) args;
-
-    pcon->run();
-
-    return nullptr;
 }

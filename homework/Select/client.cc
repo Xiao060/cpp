84f2dc94aaa679ed8dc56aac72e98c9b90c51e26
xiao <liangshengxiao1@163.com>
2023-10-11 16:30:43 +0800

    1

--- homework/Select/client.cc
+++ homework/Select/client.cc
@@ -1,4 +1,10 @@
+#include <arpa/inet.h>
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 using std::cin;
 using std::cout;
@@ -8,6 +14,23 @@ using std::endl;
 
 
 int main(int argc, char* argv[]) {
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket");
+        return -1;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(argv[2]));
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    int ret = connect(sockfd, reinterpret_cast<struct sockaddr*>(&addr), sizeof(addr));
+    if (ret == -1) {
+        perror("connect");
+        close(sockfd);
+        return -1;
+    }
 
     
 

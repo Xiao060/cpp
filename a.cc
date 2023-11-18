a26cb16ad3895b4338bbaa4ae7be0c922b7a23dd
xiao <liangshengxiao1@163.com>
2023-11-18 12:42:47 +0800

    1

--- a.cc
+++ a.cc
@@ -1,76 +1,24 @@
-#include <iostream>
-#include <string>
-#include <unistd.h>
-#include <crypt.h>
+#include <memory>
 
-#include <vector>
-#include <workflow/WFFacilities.h>
-#include <workflow/WFMySQLServer.h>
-#include <wfrest/HttpMsg.h>
-#include <workflow/MySQLMessage.h>
-#include <workflow/MySQLResult.h>
-#include <workflow/WFGlobal.h>
-#include <workflow/WFTask.h>
-#include <workflow/WFTaskFactory.h>
-#include <workflow/Workflow.h>
-#include <workflow/mysql_types.h>
+#include <iostream>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::string;
-using std::vector;
-
-WFFacilities::WaitGroup waitgroup(1);
 
 
-void mysqlLoginQueryCallback(WFMySQLTask* mysqlTask) {
-
-    // 1. 检测连接状态
-    int state = mysqlTask->get_state();
-    int error = mysqlTask->get_error();
-    if (state != WFT_STATE_SUCCESS) {
-        cout << WFGlobal::get_error_string(state, error) << endl;
-        return;
-    }
-
-
-    // 2. 检测 SQL 语句
-    protocol::MySQLResponse* resp = mysqlTask->get_resp();
-    int type = resp->get_packet_type();
-    cout << type << endl;
-    // if (type != MYSQL_PACKET_GET_RESULT) {
-    //     cout << "无查询结果!" << endl;
-    //     return;
-    // }
-
-    // 获取 mysql 中的 user_pwd
-    protocol::MySQLResultCursor cursor(resp);
-    vector<protocol::MySQLCell> row;
-    cursor.fetch_row(row);
-    string user_pwd = row[0].as_string();
-    cout << "user_pwd: " << user_pwd << endl;
-
-    // if (encodedPasswd != user_pwd) {
-    //     cout << "encodedPasswd: " << encodedPasswd << endl;
-    //     cout << "user_pwd: " << user_pwd << endl;
-    //     httpResp->String("ERROR");
-    //     return;
-    // }
-}
 
 
 int main(int argc, char* argv[]) {
 
-    string uri = "mysql://xiao:xiao060@localhost:3306/cloudisk";
-    string query = "select user_pwd from tbl_user where user_name = 'xiao060'";
+    std::auto_ptr<int>  t1(new int(10));
 
-    cout << query << endl;
-    WFMySQLTask* mysqlTask = WFTaskFactory::create_mysql_task(uri, 1, mysqlLoginQueryCallback);
-    mysqlTask->get_req()->set_query(query);
-    mysqlTask->start();
+    std::auto_ptr<int> t2(t1);
 
-    waitgroup.wait();
+    cout << t1.get() << endl;
+    //cout << *t1.get() << endl;
+    cout << t2.get() << endl;
+    cout << *t2.get() << endl;
 
     return 0;
 }

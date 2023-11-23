5e56415b8d3d7923a6096708a612fcc3007b56d4
xiao <liangshengxiao1@163.com>
2023-11-23 23:20:35 +0800

    1

--- a.cc
+++ a.cc
@@ -9,20 +9,36 @@ using std::cout;
 using std::endl;
 
 
+template<class X>
+X add(X a, X b);
 
 
 int main(int argc, char* argv[]) {
 
-    std::auto_ptr<int>  t1(new int(10));
+    // std::auto_ptr<int>  t1(new int(10));
 
-    std::auto_ptr<int> t2(t1);
+    // std::auto_ptr<int> t2(t1);
 
-    cout << t1.get() << endl;
-    //cout << *t1.get() << endl;
-    cout << t2.get() << endl;
-    cout << *t2.get() << endl;
+    // cout << t1.get() << endl;
+    // //cout << *t1.get() << endl;
+    // cout << t2.get() << endl;
+    // cout << *t2.get() << endl;
+
+    // int* p = nullptr;
+    // delete p;
+    // cout << *p << endl;
+
+    cout << add(1, 5) << endl;
+    cout << add(4.3, 5.2) << endl;
+
 
     return 0;
+}
+
+
+template<class X>
+X add(X a, X b) {
+    return a + b;
 }
 
 

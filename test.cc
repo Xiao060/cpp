e205894d50646dad53128d58bcfe1eeb4bcde320
xiao <liangshengxiao1@163.com>
2023-11-29 13:49:53 +0800

    1

--- test.cc
+++ test.cc
@@ -6,20 +6,25 @@ using std::cout;
 using std::endl;
 using std::string;
 
+const int c = 10;
 
 void test() {
     int n = 5;
     int* p = new int(10);
     string s = "Hello";
+    static int b = 20;
 
     cout << &n << endl; 
     cout << &s << endl;
     cout << p << endl;
+    cout << "c: " << &c << endl;
+    cout << "b: " << &b << endl;
 
     [n, s](int* ptr){
         // cout << n << endl;
         cout << &n << endl;
         cout << &s << endl;
+        cout << "c: " << &c << endl;
     } (p);
 }
 
